29fd9c0056521bcd6b2debb50ad67ff06a84630f
John Kappelmeier <jak485@cornell.edu>
2024-11-05 00:35:34 -0500

    Fixed Map

--- Core/Model.py
+++ Core/Model.py
@@ -74,7 +74,6 @@ class Model:
 
         # Adjust to estimate of national vote
         stateFundEst = np.array(stateFundEst)
-        stateFundEst = stateFundEst + self.geographyHead.fundEst - 0.5
 
         self.xFund = np.insert(np.array(stateFundEst), 0, self.geographyHead.fundEst)
         rho = self.correlation
